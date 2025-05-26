5c623218d83f7dcf69f0c94bd4e2d5423e2efe5e
shanxS <s#s>
2025-05-26 21:26:35 +0000

    [HW3] rest of the questions
    [WORKS] saves on container
    
    [WORKS] adding backend uri & artifact destination
    
    [WORKS] Enable volume in mlflow docker file
    
    [WORKS] Enable volume in mlflow docker file
    
    [WORKS] pivoting away from shared volumes
    
    [WORKS] pivoting away from shared volumes
    
    [WORKS] save mlflow artifact on disk
    
    [WORKS] save mlflow artifact on disk

--- 03-orchestration/hw.py
+++ 03-orchestration/hw.py
@@ -1,17 +1,24 @@
+from typing import List
 from prefect import flow, get_run_logger, task
+from prefect.context import get_run_context
 from prefect.cache_policies import INPUTS
 from prefect.docker import DockerImage
 import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
+import mlflow
+from mlflow.models import infer_signature
+import click
 
-@task(cache_policy=INPUTS)
+@task
 def data_loader(url: str = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet'):
+    mlflow.log_param("data_url", url)
     df = pd.read_parquet(url)
     get_run_logger().info(f"[Q3] num rows:{df.shape[0]} ")
+    mlflow.log_metric("input_length", df.shape[0])
     return df
 
-@task(cache_policy=INPUTS)
+@task
 def transform_data(df: pd.DataFrame):
     dv = DictVectorizer()
     
@@ -30,26 +37,45 @@ def transform_data(df: pd.DataFrame):
 
     return X, df['duration']
 
-@task(cache_policy=INPUTS)
+@task
 def train_model(X, y):
+    
+    signature = infer_signature(X, y)
     model = LinearRegression()
     model.fit(X, y)
-
     get_run_logger().info(f"[Q5] intercept of trained model:{model.intercept_} ")
+    mlflow.log_metric("intercept", model.intercept_) # type: ignore
+
+    mlflow.sklearn.log_model(
+        model, 
+        artifact_path="model",
+        signature=signature
+    )
 
 @flow
 def HW3():
-    df = data_loader()
-    X,y = transform_data(df)
-    train_model(X,y)
+    mlflow.set_experiment("HW3")
+    with mlflow.start_run(run_name=get_run_context().flow_run.name): # type: ignore
+        df = data_loader()
+        X,y = transform_data(df)
+        train_model(X,y)
+
+@click.command()
+@click.option("--remote-deploy", is_flag=True, help="Deploy to remote")
+def start(remote_deploy: bool):
+    if remote_deploy:
+        flow_name = "hw-3"
+        HW3.deploy(
+            name=flow_name,
+            work_pool_name="docker-pool",
+            image=DockerImage(
+                name=f"nexus4shashank/mlopszoomcamp:{flow_name}",
+                dockerfile="03-orchestration/prefect_base_dockerfile",
+                context="../"
+            )
+        )
+    else:
+        HW3()
 
 if __name__ == "__main__":
-    flow_name = "hw-3"
-    HW3.deploy(
-        name=flow_name,
-        work_pool_name="docker-pool",
-        image=DockerImage(
-            name=f"nexus4shashank/mlopszoomcamp:{flow_name}",
-            dockerfile="prefect_base_dockerfile",
-        )
-    )
+    start()
