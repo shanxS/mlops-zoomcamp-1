1ee15863ad0a70ae7859827187c013fd4118382f
shanxS <s#s>
2025-05-28 21:01:38 +0000

    Enabing frozen dependencies in deployed Prefect flows

--- 03-orchestration/hello_world.py
+++ 03-orchestration/hello_world.py
@@ -7,6 +7,7 @@ A simple flow that says hello.
 """
 
 from prefect import flow, get_run_logger, tags
+import click
 
 
 # The name of the flow, `hello` is inferred from the function name by default
@@ -15,10 +16,19 @@ from prefect import flow, get_run_logger, tags
 def hello(name: str = "Marvin"):
     get_run_logger().info(f"Hello, {name}!")
 
+
+@click.command()
+@click.option("--remote-deploy", is_flag=True, help="Deploy to remote")
+def start(remote_deploy: bool):
+    if remote_deploy:
+        flow_name = "hello-world-flow"
+        hello.deploy(
+            name=flow_name,
+            work_pool_name="docker-pool",
+            image=f"nexus4shashank/mlopszoomcamp:{flow_name}"
+        )
+    else:
+        hello()
+
 if __name__ == "__main__":
-    flow_name = "hello-world-flow"
-    hello.deploy(
-        name=flow_name,
-        work_pool_name="docker-pool",
-        image=f"nexus4shashank/mlopszoomcamp:{flow_name}"
-    )
+    start()
